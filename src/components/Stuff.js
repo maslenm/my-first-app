711a0dca9a89790ad68b7936de9ac2549bb18154
Matt Maslen <mattmaslen123@gmail.com>
2020-11-24 11:08:29 +0000

    q5 props working version, added 'square' prop to stuff component condition to display if true, set true by default.

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -4,17 +4,21 @@ import Square from "./Square";
 import People from "./People";
 
 
-const Stuff = () => (
+const Stuff = ( { square } ) => (
     <>
     <Header text="Hello, world!" />
     
     <Paragraph message ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos." />
     
-    <Square  colour="red"/> 
-    
+    {square ? (<Square colour="red" /> ) : null }
+
     <People names={["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"]}/>
     </>
 );
+
+Stuff.defaultProps = {
+    square: true,
+};
 
 export default Stuff;
 
