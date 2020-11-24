d8f4fbb929140a729ac60892101dc97087bf8fcc
Matt Maslen <mattmaslen123@gmail.com>
2020-11-24 09:49:21 +0000

    q2 props working version <paragraph> to use 'message' prop

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -7,12 +7,16 @@ import People from "./People";
 const Stuff = () => (
     <>
     <Header text="Hello, world!" />
-    <Paragraph />
-    <Square />
-    <People />
-
     
+    <Paragraph message ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos." />
+    
+    <Square  colour="red"/> 
+    
+    <People />
     </>
 );
 
 export default Stuff;
+
+//bgColour={bgColour}
+    
