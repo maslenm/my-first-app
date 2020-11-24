e7843be94cd849e3637d175e99f36e402bdbcdf1
Matt Maslen <mattmaslen123@gmail.com>
2020-11-24 10:01:00 +0000

    q3 working version updated div style background colour to use prop 'colour'

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,11 @@
-const Square = () => (
-    <div style={{ backgroundColor: "red", height: "200px", width: "200px" }}></div>
-);
-
-export default Square;
+const Square = ({ colour }) => (
+    <div style={{ backgroundColor: colour, height: "200px", width: "200px" }}></div>
+    );
+    
+    
+    Square.defaultProps = {
+        colour: "blue",
+    };
+    
+    export default Square;
+    
