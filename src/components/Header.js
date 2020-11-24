c9431e92cbe0bd30ab67cce1f106cd11b230d55b
Matt Maslen <mattmaslen123@gmail.com>
2020-11-24 11:24:39 +0000

    cleaned up file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,6 @@
-/* const Header = ({ text }) => (
-    <h1>{ text }</h1>
-);
-   */
-
 const Header = ({ children }) => (
     <h1>{ children }</h1>
 );
   
 export default Header;
-
-//Update your <Header> component so that it can accept a text prop, which it will display between the <h1> tags
 
