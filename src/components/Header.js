0d7f1fd399c81ba8faffbd6dbd529726011ce06d
Matt Maslen <mattmaslen123@gmail.com>
2020-11-24 13:13:02 +0000

    updated components to be class based components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,20 @@
-const Header = ({ children }) => (
-    <h1>{ children }</h1>
-);
-  
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        let { children } = this.props;
+        
+        return (
+            <h1>{ children }</h1>
+        );
+    };
+};
+
 export default Header;
+
+/* const Header = ({ children }) => (
+    <h1>{ children }</h1>
+); */
+  
+
 
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,9 +1,22 @@
-const Paragraph = ({ children }) => (
+import React from "react";
+
+class Paragraph extends React.Component {
+    render() {
+        let { children } = this.props;
+
+        return (
+            <p>{ children ? children : "Hello, world." }</p>
+        );
+    };   
+};
+
+export default Paragraph;
+
+
+/* const Paragraph = ({ children }) => (
     <p>
         { 
             children ? children : "Hello, world."
         }
     </p>
-);
-
-export default Paragraph;
+); */
