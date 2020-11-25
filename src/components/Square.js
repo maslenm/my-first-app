1bc4bfc10b0160e2f31d203c4ea771f1ad0b1095
Matt Maslen <mattmaslen123@gmail.com>
2020-11-25 09:36:03 +0000

    q2 working version of state challenges, square alternates colour on click, starts green

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,86 @@
-const Square = ({ colour }) => (
-    <div style={{ backgroundColor: colour, height: "200px", width: "200px" }}></div>
+import React from "react";
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bgGreen: true,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    };
+
+
+
+    handleClick() {
+        this.setState({ bgGreen: !this.state.bgGreen });
+    }
+
+    render() {
+        let {colour} = this.props;
+        let colourState = this.state.bgGreen ? "green" :  colour;
+
+        return (
+            <div onClick={ this.handleClick } style={{ 
+                backgroundColor: colourState, 
+                height: "200px", 
+                width: "200px", 
+            }}></div>
+        )
+    }
+};
+
+Square.defaultProps = {
+    colour: "blue",
+};
+    
+export default Square;
+
+
+
+
+/* class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bgColor: 
+        };
+        this.handleClick = this.handleClick.bind(this);
+    };
+
+
+
+    handleClick() {
+        let {colour} = this.props; 
+        
+        this.setState({ bgColor: colour });
+    }
+
+    render() {
+
+        return (
+            <div onClick={ this.handleClick } style={{ 
+                backgroundColor: this.state.bgColor, 
+                height: "200px", 
+                width: "200px", 
+            }}></div>
+        )
+    }
+}; */
+
+
+
+    
+
+    
+/* const Square = ({ colour }) => (
+    <div style={{ backgroundColor: colour, 
+        height: "200px", 
+        width: "200px", 
+    }}></div>
     );
     
     
     Square.defaultProps = {
         colour: "blue",
     };
-    
-    export default Square;
-    
+ */
